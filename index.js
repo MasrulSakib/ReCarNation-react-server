7856dc3e81958abb6eedea97e462e702895cdcb9
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-05-24 22:23:45 +0600

    carsCollention added, categorized and loaded from mongodb

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+//midddleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.SECRET_NAME}:${process.env.SECRET_PASSWORD}@cluster1.m4sihj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, }
+});
+
+async function run() {
+    try {
+        const carsCollection = client.db('ReCarNation').collection('carsCollection')
+
+        app.get('/cars/:company', async (req, res) => {
+            company = req.params.company
+            const query = { company: company };
+            const cars = await carsCollection.find(query).toArray()
+            res.send(cars);
+
+        })
+
+
+    } finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
+
+
+app.get('/', (req, res) => {
+    res.send('recarnation is running successfully')
+})
+
+app.listen(port, () => {
+    console.log(`ReCarNation is running on port ${port}`)
+})
